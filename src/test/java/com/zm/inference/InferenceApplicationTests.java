406846c977a1b62073bf3bb5fe407cd748738052
zm <zhoumiao48@gmail.com>
2020-05-12 13:05:35 +0800

    项目初始化

---
+++ src/test/java/com/zm/inference/InferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zm.inference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InferenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
